7c57d4c54c8bdf0dd63b81903d95cd268d0fd18b
alialibalaei <ali.alibalaii778@gmail.com>
2025-01-08 11:48:50 +0330

    FBFF-36: change subject insight path.

--- apps/assessment/views/assessment_insight_views.py
+++ apps/assessment/views/assessment_insight_views.py
@@ -33,6 +33,10 @@ class AssessmentSubjectInsightApi(APIView):
             return Response(status=result["status_code"])
         return Response(data=result["body"], status=result["status_code"])
 
+
+class AssessmentSubjectLoadInsightApi(APIView):
+    permission_classes = [IsAuthenticated]
+
     def get(self, request, assessment_id, subject_id):
         result = assessment_insight_services.get_assessment_subject_insights(request, assessment_id, subject_id)
         return Response(data=result["body"], status=result["status_code"])
