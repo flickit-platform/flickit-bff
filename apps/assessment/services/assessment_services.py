7ccacc5177acf7f21491f3c8c0afb4c2626a30c2
Maziyar Gerami <maziyar.gerami@connect2wow.com>
2025-01-21 14:57:56 +0330

    FBFF-46: Fix method

--- apps/assessment/services/assessment_services.py
+++ apps/assessment/services/assessment_services.py
@@ -139,7 +139,7 @@ def get_report_metadata(request, assessment_id):
     return {"Success": True, "body": response.json(), "status_code": response.status_code}
 
 def update_report_metadata(request, assessment_id):
-    response = requests.get(
+    response = requests.patch(
         ASSESSMENT_URL + f'assessment-core/api/assessments/{assessment_id}/report-metadata',
         headers={'Authorization': request.headers['Authorization']})
     return {"Success": True, "body": response.json(), "status_code": response.status_code}    
