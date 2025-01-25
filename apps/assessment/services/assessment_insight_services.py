5d71ea268be42f6e8d258ccbb07253a60221d5d5
Maziyar Gerami <maziyar.gerami@connect2wow.com>
2025-01-25 13:45:11 +0330

    FBFF-50: Refactor - Remove init subject insight from `get_assessment_subject_insights` service

--- apps/assessment/services/assessment_insight_services.py
+++ apps/assessment/services/assessment_insight_services.py
@@ -74,26 +74,7 @@ def get_assessment_insights(request, assessment_id):
     return load_result
 
 
-def _load_subject_insights(request, assessment_id, subject_id):
-    response = requests.get(
-        ASSESSMENT_URL + f'assessment-core/api/assessments/{assessment_id}/subjects/{subject_id}/insight',
-        headers={'Authorization': request.headers.get('Authorization')}
-    )
-
-    if response.status_code != 200:
-        return {
-            "Success": False,
-            "body": response.json(),
-            "status_code": response.status_code
-        }
-    return {
-        "Success": True,
-        "body": response.json(),
-        "status_code": response.status_code
-    }
-
-
-def init_subject_insights(request, assessment_id, subject_id):
+def get_subject_insights(request, assessment_id, subject_id):
     response = requests.post(
         ASSESSMENT_URL + f'assessment-core/api/assessments/{assessment_id}/subjects/{subject_id}/init-insight',
         json=request.data,
@@ -113,27 +94,21 @@ def init_subject_insights(request, assessment_id, subject_id):
 
 
 def get_assessment_subject_insights(request, assessment_id, subject_id):
-    load_result = _load_subject_insights(request, assessment_id, subject_id)
-
-    if not load_result["Success"]:
-        return load_result
-
-    data = load_result["body"]
-    default_insight = data.get('defaultInsight')
-    assessor_insight = data.get('assessorInsight')
-
-    if not default_insight and not assessor_insight:
-        init_result = init_subject_insights(request, assessment_id, subject_id)
-        if not init_result["Success"]:
-            error_code = init_result["body"].get("code")
-            if error_code == "INVALID_INPUT":
-                return _load_subject_insights(request, assessment_id, subject_id)
-            else:
-                return init_result
-
-        return _load_subject_insights(request, assessment_id, subject_id)
-
-    return load_result
+    response = requests.get(
+        ASSESSMENT_URL + f'assessment-core/api/assessments/{assessment_id}/subjects/{subject_id}/insight',
+        headers={'Authorization': request.headers.get('Authorization')}
+    )
+    if response.status_code != 200:
+        return {
+            "Success": False,
+            "body": response.json(),
+            "status_code": response.status_code
+        }
+    return {
+        "Success": True,
+        "body": response.json(),
+        "status_code": response.status_code
+    }
 
 
 def create_assessment_subject_insights(request, assessment_id, subject_id):
