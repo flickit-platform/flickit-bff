301ea4c9a3b740477ac28344d635eec768745b4c
Maziyar Gerami <maziyar.gerami@connect2wow.com>
2025-02-16 18:48:25 +0330

    FBFF-60: Refactor : Remove useless APIs

--- apps/assessment/services/assessment_insight_services.py
+++ apps/assessment/services/assessment_insight_services.py
@@ -68,17 +68,6 @@ def get_assessment_subject_insights(request, assessment_id, subject_id):
     }
 
 
-def create_assessment_subject_insights(request, assessment_id, subject_id):
-    response = requests.post(
-        ASSESSMENT_URL + f'assessment-core/api/assessments/{assessment_id}/insights/subjects/{subject_id}',
-        json=request.data,
-        headers={'Authorization': request.headers['Authorization'],
-                 'Accept-Language': request.headers['Accept-Language']})
-    if response.status_code == 200:
-        return {"Success": True, "body": None, "status_code": response.status_code}
-    return {"Success": False, "body": response.json(), "status_code": response.status_code}
-
-
 def create_assessment_subject_insight(request, assessment_id, subject_id):
     response = requests.post(
         ASSESSMENT_URL + f'assessment-core/api/assessments/{assessment_id}/subjects/{subject_id}/insight',
