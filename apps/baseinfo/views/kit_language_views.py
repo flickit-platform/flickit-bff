fc36d96a9d66e46e8f4592b2e7727219ce6e594a
Maziyar Gerami <maziyar.gerami@connect2wow.com>
2025-02-17 11:00:36 +0330

    FBFF-63: Add Authentication to view

--- apps/baseinfo/views/kit_language_views.py
+++ apps/baseinfo/views/kit_language_views.py
@@ -1,3 +1,4 @@
+from rest_framework.permissions import IsAuthenticated
 from rest_framework.views import APIView
 from rest_framework.response import Response
 
@@ -5,6 +6,7 @@ from baseinfo.services import kit_language_services
 
 
 class KitLanguageViews(APIView):
+    permission_classes = [IsAuthenticated]
 
     def get(self, request):
         result = kit_language_services.load_kit_languages(request)
