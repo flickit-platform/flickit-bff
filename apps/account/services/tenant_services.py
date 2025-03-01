dbeb471dd286124da8c9e1a3b3b2ae26cb0836bc
Maziyar Gerami <maziyar.gerami@connect2wow.com>
2025-03-01 09:45:56 +0330

    FBFF-71: Feat: Add `Accept-Language` header to `get_tenant` and remove `tenant_info` and `tenant_info`

--- apps/account/services/tenant_services.py
+++ apps/account/services/tenant_services.py
@@ -2,19 +2,8 @@ import requests
 from assessmentplatform.settings import ASSESSMENT_URL
 
 
-def tenant_info():
+def get_tenant(request):
     response = requests.get(
-        ASSESSMENT_URL + 'assessment-core/api/tenant/info')
-    return {"Success": True, "body": response.json(), "status_code": response.status_code}
-
-
-def tenant_logo():
-    response = requests.get(
-        ASSESSMENT_URL + 'assessment-core/api/tenant/logo')
-    return {"Success": True, "body": response.json(), "status_code": response.status_code}
-
-
-def get_tenant():
-    response = requests.get(
-        ASSESSMENT_URL + 'assessment-core/api/tenant')
+        ASSESSMENT_URL + 'assessment-core/api/tenant',
+        headers={'Accept-Language': request.headers['Accept-Language']})
     return {"Success": True, "body": response.json(), "status_code": response.status_code}
