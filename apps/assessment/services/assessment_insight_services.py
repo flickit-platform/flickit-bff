b3b48ad74842a1ae4aada5f4303029a4d413ec87
Maziyar Gerami <maziyar.gerami@connect2wow.com>
2025-03-02 08:10:08 +0330

    FBFF-73: Feat: Add `approve_expired_assessment_insights` to services

--- apps/assessment/services/assessment_insight_services.py
+++ apps/assessment/services/assessment_insight_services.py
@@ -201,3 +201,14 @@ def regenerate_expired_assessment_insights(request, assessment_id):
     if response.status_code == 200:
         return {"Success": True, "body": None, "status_code": response.status_code}
     return {"Success": False, "body": response.json(), "status_code": response.status_code}
+
+
+def approve_expired_assessment_insights(request, assessment_id):
+    response = requests.put(
+        ASSESSMENT_URL + f'assessment-core/api/assessments/{assessment_id}/approve-expired-insights',
+        headers={'Authorization': request.headers['Authorization'],
+                 'Accept-Language': request.headers['Accept-Language']})
+    if response.status_code == 200:
+        return {"Success": True, "body": None, "status_code": response.status_code}
+    return {"Success": False, "body": response.json(), "status_code": response.status_code}
+
