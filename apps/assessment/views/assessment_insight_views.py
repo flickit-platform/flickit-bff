61a430f91807bd0c227b8d83e01b3109e83644ba
Maziyar Gerami <maziyar.gerami@connect2wow.com>
2025-03-02 08:09:28 +0330

    FBFF-73: Feat: Add `ApproveExpiredAssessmentInsightsApi` to views

--- apps/assessment/views/assessment_insight_views.py
+++ apps/assessment/views/assessment_insight_views.py
@@ -141,3 +141,13 @@ class RegenerateExpiredAssessmentInsightsApi(APIView):
         if result["Success"]:
             return Response(status=result["status_code"])
         return Response(data=result["body"], status=result["status_code"])
+
+
+class ApproveExpiredAssessmentInsightsApi(APIView):
+    permission_classes = [IsAuthenticated]
+
+    def put(self, request, assessment_id):
+        result = assessment_insight_services.regenerate_expired_assessment_insights(request, assessment_id)
+        if result["Success"]:
+            return Response(status=result["status_code"])
+        return Response(data=result["body"], status=result["status_code"])
