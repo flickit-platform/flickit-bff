4ccf16479b2afa3f2283f8bcf618f6bfe7205925
Maziyar Gerami <maziyar.gerami@connect2wow.com>
2025-03-02 07:53:44 +0330

    FBFF-70: Fix: Minor Improvement

--- apps/baseinfo/services/check_create_space_services.py
+++ apps/baseinfo/services/check_create_space_services.py
@@ -7,4 +7,6 @@ def check_allowance(request):
         ASSESSMENT_URL + f'assessment-core/api/check-create-space',
         headers={'Authorization': request.headers['Authorization'],
                  'Accept-Language': request.headers['Accept-Language']})
-    return {"Success": True, "body": response.json(), "status_code": response.status_code}
+    if response.status_code == 200:
+        return {"Success": True, "body": response.json(), "status_code": response.status_code}
+    return {"Success": False, "body": response.json(), "status_code": response.status_code}
