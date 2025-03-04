1e7c19e3631c5915b5a61ef0d7b55c9bcf9eb4a3
Maziyar Gerami <maziyar.gerami@connect2wow.com>
2025-03-04 18:06:15 +0330

    FBFF-75: Feat: Add service

--- apps/assessment/services/comments_services.py
+++ apps/assessment/services/comments_services.py
@@ -1,3 +1,5 @@
+from http import HTTPStatus
+
 import requests
 
 from assessmentplatform.settings import ASSESSMENT_URL
@@ -10,3 +12,13 @@ def get_list_comments(request):
         headers={'Authorization': request.headers['Authorization'],
                  'Accept-Language': request.headers['Accept-Language']})
     return {"Success": True, "body": response.json(), "status_code": response.status_code}
+
+def resolve_assessment_comments(request, assessment_id):
+    response = requests.put(
+        ASSESSMENT_URL + f'assessment-core/api/assessments/{assessment_id}/resolve-comments',
+        params=request.query_params,
+        headers={'Authorization': request.headers['Authorization'],
+                 'Accept-Language': request.headers['Accept-Language']})
+    if response.status_code != HTTPStatus.OK:
+        return {"Success": False, "body": response.json(), "status_code": response.status_code}
+    return {"Success": True, "body": None, "status_code": response.status_code}
