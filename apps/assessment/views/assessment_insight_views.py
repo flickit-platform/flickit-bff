a436297b4facb6fab1af9c0a8a34aa736fbfe7f9
Maziyar Gerami <maziyar.gerami@connect2wow.com>
2025-03-11 16:05:08 +0330

    FBFF-78: Refactor: Remove extra codes

--- apps/assessment/views/assessment_insight_views.py
+++ apps/assessment/views/assessment_insight_views.py
@@ -145,8 +145,6 @@ class RegenerateExpiredAssessmentInsightsApi(APIView):
 
     def put(self, request, assessment_id):
         result = assessment_insight_services.regenerate_expired_assessment_insights(request, assessment_id)
-        if result["Success"]:
-            return Response(status=result["status_code"])
         return Response(data=result["body"], status=result["status_code"])
 
 
