3a26bd09ea0eaa20528007b4fa70f68d0a711b3f
Maziyar Gerami <maziyar.gerami@connect2wow.com>
2025-05-05 17:53:46 +0330

    FBFF-89: Add Service

--- apps/assessment/services/assessment_report_services.py
+++ apps/assessment/services/assessment_report_services.py
@@ -41,3 +41,14 @@ def report_publish_status(request, assessment_id):
     if response.status_code == 200:
         return {"Success": True, "body": None, "status_code": response.status_code}
     return {"Success": False, "body": response.json(), "status_code": response.status_code}
+
+
+def report_visibility_status(request, assessment_id):
+    response = requests.put(ASSESSMENT_URL +
+                            f'assessment-core/api/assessments/{assessment_id}/report-visibility-status',
+                            json=request.data,
+                            headers={'Authorization': request.headers['Authorization'],
+                                     'Accept-Language': request.headers['Accept-Language']})
+    if response.status_code == 200:
+        return {"Success": True, "body": response.json(), "status_code": response.status_code}
+    return {"Success": False, "body": response.json(), "status_code": response.status_code}
