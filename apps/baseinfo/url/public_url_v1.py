1fe3e9ae29b6b4b2d16c4240b46cc602d807bc63
Maziyar Gerami <maziyar.gerami@connect2wow.com>
2025-05-11 18:22:55 +0330

    FBFF-92: Fix url

--- apps/baseinfo/url/public_url_v1.py
+++ apps/baseinfo/url/public_url_v1.py
@@ -5,5 +5,5 @@ from baseinfo.views import assessment_kit_views
 
 urlpatterns = [
     path("graphical-report/<uuid:link_hash>/", assessment_report_views.PublicGraphicalReportApi.as_view()),
-    path("assessment-kits/<str:assessment_kit_id>", assessment_kit_views.PublicAssessmentKitApi.as_view()),
+    path("assessment-kits/<str:assessment_kit_id>/", assessment_kit_views.PublicAssessmentKitApi.as_view()),
 ]
