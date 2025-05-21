b24e97194bb5be52d5a1e104680bfd0b842e95d5
Maziyar Gerami <maziyar.gerami@connect2wow.com>
2025-05-21 08:12:08 +0330

    FAC-95: Modify expected status_code from 201 to 200

--- apps/assessment/services/assessment_report_services.py
+++ apps/assessment/services/assessment_report_services.py
@@ -66,6 +66,6 @@ def prepare_assessment_report(request, assessment_id):
                             json=request.data,
                             headers={'Authorization': request.headers['Authorization'],
                                      'Accept-Language': request.headers['Accept-Language']})
-    if response.status_code == 201:
+    if response.status_code == 200:
         return {"Success": True, "body": None, "status_code": response.status_code}
     return {"Success": False, "body": response.json(), "status_code": response.status_code}
