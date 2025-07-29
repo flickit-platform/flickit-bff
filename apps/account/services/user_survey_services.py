0f761a950d5b625599fe9f370e07ac6246fa753a
Maziyar Gerami <maziyar.gerami@connect2wow.com>
2025-07-29 14:29:39 +0330

    FBFF-106: Add user_survey_services.py

---
+++ apps/account/services/user_survey_services.py
@@ -0,0 +1,14 @@
+import requests
+
+from assessmentplatform.auth.auth_header_provider import AuthHeaderProvider
+from assessmentplatform.settings import ASSESSMENT_URL
+
+
+def set_dont_show_again(request):
+    response = requests.post(
+        ASSESSMENT_URL + f'assessment-core/api/user-surveys/dont-show-again',
+        json=request.data,
+        headers=AuthHeaderProvider(request).get_headers())
+    if response.status_code == 200:
+        return {"Success": True, "body": None, "status_code": response.status_code}
+    return {"Success": False, "body": response.json(), "status_code": response.status_code}
