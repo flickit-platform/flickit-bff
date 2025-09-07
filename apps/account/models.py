5ff3fbe62e3fbc08e3d06b28888c05f8032fc93f
Maziyar Gerami <maziyar.gerami@connect2wow.com>
2025-09-07 07:59:06 +0330

    Revert "Merge pull request #111 from flickit-platform/FBFF-110_modify-services-use-user-entity"
    This reverts commit ec89f33566d6f2eb6e8031498e725b9995510c01, reversing
    changes made to b64225094713fe48c91fb00ff35f0f9fc1760081.

--- apps/account/models.py
+++ apps/account/models.py
@@ -25,9 +25,19 @@ class CustomUserManager(BaseUserManager):
         return user
 
     def create_user(self, email, password, display_name, **extra_fields):
+        extra_fields.setdefault("is_staff", False)
+        extra_fields.setdefault("is_superuser", False)
         return self._create_user(email, password, display_name, **extra_fields)
 
     def create_superuser(self, email, password, display_name, **extra_fields):
+        extra_fields.setdefault("is_staff", True)
+        extra_fields.setdefault("is_superuser", True)
+
+        if extra_fields.get("is_staff") is not True:
+            raise ValueError("Superuser must have is_staff=True.")
+        if extra_fields.get("is_superuser") is not True:
+            raise ValueError("Superuser must have is_superuser=True.")
+
         return self._create_user(email, password, display_name, **extra_fields)
 
 
@@ -35,7 +45,10 @@ class User(AbstractBaseUser, PermissionsMixin):
     id = models.UUIDField(primary_key=True, default=uuid4)
     email = models.EmailField(unique=True, max_length=254,
                               error_messages={'unique': "A user with this email address already exists."})
+    is_active = models.BooleanField(default=True)
     display_name = models.CharField(max_length=255)
+    is_superuser = models.BooleanField(default=False)
+    is_staff = models.BooleanField(default=False)
     picture = models.ImageField(upload_to='user/images', null=True, validators=[validate_file_size])
     bio = models.CharField(null=True, max_length=400)
     linkedin = models.URLField(null=True, blank=True)
